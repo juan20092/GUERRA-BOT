75e0e4f8978327297af943694a1ad376964576b6
guerraroncancio-stack <guerraroncancio@gmail.com>
2026-04-03 00:42:31 -0500

    Update group message format in grupo-todos.js

--- plugins/grupo-todos.js
+++ plugins/grupo-todos.js
@@ -4,22 +4,24 @@ let handler = async (m, { conn, participants, groupMetadata, text }) => {
   let usuarios = participants.map(v => v.id)
   let total = participants.length
   let groupName = groupMetadata.subject
-
   let mensaje = text ? text : 'Atención grupo'
 
-  let teks = `┏━━━━━━━━━━━┓
-┃  🛸 𝖦𝗋𝗎𝗉𝗈: ${groupName}
-┃  👥 𝖬𝗂𝖾𝗆𝖻𝗋𝗈𝗌: ${total}
-┗━━━━━━━━━━━┛
-┌──⭓ *${mensaje}*\n`
+  let teks = `╭━━━〔 🚀 LLAMADO GENERAL 🚀 〕━━━⬣
+┃ 🏷️ Grupo: ${groupName}
+┃ 👥 Miembros: ${total}
+┃ 📢 Aviso: ${mensaje}
+╰━━━━━━━━━━━━━━━━⬣
+
+┏━━━〔 👑 INVOCADOS 👑 〕━━━⬣
+`
 
   for (let user of participants) {
-    teks += `🍷 @${user.id.split('@')[0]}\n`
+    teks += `┃ ✦ @${user.id.split('@')[0]}\n`
   }
 
-  teks += `└───────⭓
+  teks += `┗━━━━━━━━━━━━━━━━⬣
 
-𝘎𝘜𝘌𝘙𝘙𝘈 𝘉𝘖𝘛 👑`
+⚡ GUERRA BOT ACTIVADO ⚡`
 
   await conn.sendMessage(m.chat, {
     text: teks,
