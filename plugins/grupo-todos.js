ca77e96bc846d558b63c48fb59c0c957799cef27
guerraroncancio-stack <guerraroncancio@gmail.com>
2026-04-03 00:30:01 -0500

    Add group mention handler for general announcements

---
+++ plugins/grupo-todos.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, participants }) => {
+  if (!m.isGroup) return m.reply('❌ Este comando solo funciona en grupos')
+
+  let teks = '📢 *Mención general del grupo*\n\n'
+
+  for (let user of participants) {
+    teks += `⭔ @${user.id.split('@')[0]}\n`
+  }
+
+  await conn.sendMessage(m.chat, {
+    text: teks,
+    mentions: participants.map(v => v.id)
+  }, { quoted: m })
+}
+
+handler.customPrefix = /^(\.?todos)$/i
+handler.command = new RegExp
+
+export default handler
